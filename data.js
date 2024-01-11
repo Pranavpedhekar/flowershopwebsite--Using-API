4a7a58558d0fdbd26feca18714f3adfa079544a9
Pranav <pranavpedhekar13@gmail.com>
2024-01-11 23:21:13 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,52 @@
+let shopItemsData = [
+    {
+    "id": 1,
+    "name": "White Lily N Glass Vase",
+    "price": 109.95,
+    "img": "./images/product1.jpg"
+},
+{
+    "id": 2,
+    "name": "Pink Opulence",
+    "price": 22.3,
+    "img": "./images/product2.jpg"
+}, 
+{
+    "id": 3,
+    "name": "Classic 10 Red Roses Bouquet",
+    "price": 55.99,
+    "img": "./images/product3.jpg"
+}, 
+{
+    "id": 4,
+    "name": "Bellina Purple Orchid Bouquet",
+    "price": 15.99,
+    "img": "./images/product4.jpg"
+}, 
+{
+    "id": 5,
+    "name": "Sunshine Gerberas For Celebration ",
+    "price": 168,
+    "img": "./images/product5.jpg"
+}, 
+{
+    "id": 6,
+    "name": "Forever 12 Red Rose Bunch",
+    "price": 168,
+    "img": "./images/product6.jpg"
+    
+}, {
+    "id": 7,
+    "name": "Luminous Lilies in Blue",
+    "price": 9.99,
+    "img": "./images/product7.jpg"
+  
+}, 
+{
+    "id": 8,
+    "name": "White Pink Floral Extravaganza",
+    "price": 10.99,
+    "img": "./images/product8.jpg"
+   
+}
+]
